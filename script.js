2f448c3df24cdebf44bc4a5452a27dca441af3c7
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-17 15:17:18 +0100

    milestone 0 completato

---
+++ script.js
@@ -0,0 +1,46 @@
+let team = [
+
+    barnett = {
+
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        foto: "wayne-barnett-founder-ceo.jpg"
+    },
+    
+    caroll = {
+    
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto: "angela-caroll-chief-editor.jpg"
+    },
+    
+    gordon = {
+    
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        foto: "walter-gordon-office-manager.jpg"
+    },
+    
+    lopez = {
+    
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        foto: "angela-lopez-social-media-manager.jpg"
+    },
+    
+    estrada = {
+    
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        foto: "scott-estrada-developer.jpg"
+    },
+    
+    ramos = {
+    
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        foto: "barbara-ramos-graphic-designer.jpg"
+    }
+
+]
+
