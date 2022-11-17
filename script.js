fe0cb32e4ee8dba59d6cbcf60baf656412ab4dd5
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-17 15:56:19 +0100

    completato milestone 1

--- script.js
+++ script.js
@@ -1,46 +1,75 @@
+//Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+//MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+
 let team = [
 
-    barnett = {
+    barnett ={
 
         nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
         foto: "wayne-barnett-founder-ceo.jpg"
     },
     
-    caroll = {
+    carol ={
     
         nome: "Angela Caroll",
         ruolo: "Chief Editor",
         foto: "angela-caroll-chief-editor.jpg"
     },
     
-    gordon = {
+    gordon ={
     
         nome: "Walter Gordon",
         ruolo: "Office Manager",
         foto: "walter-gordon-office-manager.jpg"
     },
     
-    lopez = {
+    lopez ={
     
         nome: "Angela Lopez",
         ruolo: "Social Media Manager",
         foto: "angela-lopez-social-media-manager.jpg"
     },
     
-    estrada = {
+    estrada ={
     
         nome: "Scott Estrada",
         ruolo: "Developer",
         foto: "scott-estrada-developer.jpg"
     },
     
-    ramos = {
+    ramos ={
     
         nome: "Barbara Ramos",
         ruolo: "Graphic Designer",
         foto: "barbara-ramos-graphic-designer.jpg"
     }
 
-]
+];
 
+//MILESTONE 1: Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+
+stampaTeam (team);
+
+
+
+function stampaTeam (team) {
+
+    for (let i=0; i<team.length; i++) {
+
+        console.log(
+            `
+            -------------------MEMBRO------------------------
+
+                Nome: ${team[i].nome}
+                Ruolo: ${team[i].ruolo}
+                Foto: ${team[i].foto}
+                
+            -------------------------------------------------
+            `
+        )
+    };
+    
+}
